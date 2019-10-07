d79b18e1d4b5ab5de803d45f04761f5bd9575407
Vano Devium <ivan.kapshtyk@herobrands.com.au>
2019-10-07 12:18:59 +0300

    Added basic condition for different Laravel versions

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,10 +8,21 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
 {
+    public const VERSION5 = 5;
+    public const VERSION6 = 6;
+
     public function boot()
     {
+        $version = (int) $this->app->version();
+
+        $from = __DIR__.'/../config/.php_cs';
+
+        if (in_array($version, [self::VERSION5, self::VERSION6], true)) {
+            $from .= $version;
+        }
+
         $this->publishes([
-            __DIR__.'/../config/.php_cs' => base_path('.php_cs'),
+            $from => base_path('.php_cs'),
         ], 'config');
     }
 }
