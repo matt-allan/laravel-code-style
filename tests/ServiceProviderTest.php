51efc868f934157fb63812d8f48406df30251f0c
Markus Podar <markus@fischer.name>
2020-09-07 16:57:29 +0200

    Add support for Laravel 8 (#29)
    * Add support for Laravel 8
    
    * composer: allow installing dev packages, but prefer stable
    
    * composer: add orchestra/testbench, requires bumping phpunit too
    
    * tests: show that publishing works
    
    * gha: expand test matrix to test support Laravel versions

---
+++ tests/ServiceProviderTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MattAllan\LaravelCodeStyle;
+
+use Illuminate\Filesystem\Filesystem;
+use Mockery;
+use Orchestra\Testbench\TestCase;
+
+class ServiceProviderTest extends TestCase
+{
+    public function test_service_provider_boots(): void
+    {
+        $mockFileSystem = $this->mock(Filesystem::class);
+        $this->instance('files', $mockFileSystem);
+
+        $mockFileSystem
+            ->shouldReceive('isFile')
+            ->with(Mockery::on(static function ($a) {
+                return strpos($a, 'config/.php_cs') > 0;
+            }))
+            ->andReturn(true)
+            ->once();
+        $mockFileSystem->shouldReceive('exists')->andReturn(false)->once();
+        $mockFileSystem->shouldReceive('isDirectory')->andReturn(true)->once();
+        $mockFileSystem->shouldReceive('copy')->once();
+
+        $this->artisan('vendor:publish', [
+            '--provider' => ServiceProvider::class,
+        ]);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            ServiceProvider::class,
+        ];
+    }
+}
